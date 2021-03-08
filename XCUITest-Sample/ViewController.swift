4839914d7fb751ca69b8601051d482abead12fd2
t-osawa-009 <da87435@gmail.com>
2021-03-08 15:53:35 +0900

    Initial Commit

---
+++ XCUITest-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCUITest-Sample
+//
+//  Created by takuya osawa on 2021/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
